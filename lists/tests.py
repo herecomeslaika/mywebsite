ff47012fc875633e9595acc0d7c211216ce2b835
杰拉德 <447121475@bupt.edu.cn>
2026-04-13 09:19:25 +0800

    message 4 first unit test

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
-class SmokeTest(TestCase):
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
-# Create your tests here.
+from django.urls import resolve
+from lists.views import home_page  # (1) 注意这里：改成了从 lists 应用导入
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')  # (2)
+        self.assertEqual(found.func, home_page)  # (3)
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-
+def home_page(request):
+    pass
 # Create your views here.
