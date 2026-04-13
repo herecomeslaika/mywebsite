d4809fa5f789c9f1e5ba1776647c277de848e929
杰拉德 <447121475@bupt.edu.cn>
2026-04-13 08:29:00 +0800

    重构：将基础测试脚本升级为基于 unittest 的 functional_tests.py

--- test_selenium.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-
-# 1. 启动一个具体的浏览器实例（这里以 Chrome 为例）
-browser = webdriver.Chrome()
-
-# 2. 输入网址，确保这里所有的标点符号（包括冒号和斜杠）都是英文状态
-browser.get('http://localhost:8000')
-assert 'Django' in browser.page_source
