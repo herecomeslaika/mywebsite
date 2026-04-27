4ee5ec6f32c0dcce6de5cbe02525762c83902ac6
杰拉德 <447121475@bupt.edu.cn>
2026-04-27 08:12:05 +0800

    扩展功能测试，要求网页能够记住并显示多个待办事项

--- lists/tests.py
+++ lists/tests.py
@@ -3,8 +3,13 @@ from django.test import TestCase
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
-        # 1. 使用 Django 内置的模拟客户端，像真人一样去访问首页
         response = self.client.get('/')
-        
-        # 2. 一键断言：检查这个响应是不是使用了名叫 'home.html' 的模板渲染出来的
         self.assertTemplateUsed(response, 'home.html')
+
+    # 新增的测试方法：专门测试 POST 请求
+    def test_can_save_a_POST_request(self):
+        # 1. 模拟发送一个 POST 请求，假装用户在输入框提交了 'A new list item'
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        
+        # 2. 检查网页的 HTML 源码中，是否包含了我们刚刚提交的那句话
+        self.assertIn('A new list item', response.content.decode())
