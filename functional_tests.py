c5763ec1442e9eabebc7ce5fec264daa695456ca
杰拉德 <447121475@bupt.edu.cn>
2026-04-27 08:28:19 +0800

    重构 functional_tests，提取辅助方法 check_for_row_in_list_table

--- functional_tests.py
+++ functional_tests.py
@@ -12,51 +12,45 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    # 这是我们新提取的“辅助工具”
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # 张三听说有一个在线待办事项的应用
-        # 他去看了这个应用的首页
         self.browser.get('http://localhost:8000')
 
-        # 他注意到网页的标题和头部都包含“To-Do”这个词
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text  # (1)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
 
-        # 应用有一个输入待办事项的文本输入框
-        inputbox = self.browser.find_element(By.ID, 'id_new_item')  # (1)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )
 
-        # 他在文本输入框中输入了“Buy flowers”
-        inputbox.send_keys('Buy flowers')  # (2)
+        # 他输入了“Buy flowers”并按回车
+        inputbox.send_keys('Buy flowers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        # 他按了回车键键后，页面更新了
-        # 待办事项表格中显示了“1: Buy flowers”
-        inputbox.send_keys(Keys.ENTER)  # (3)
-        time.sleep(1)  # (4)
+        # 【重点修改区 1】：使用新工具检查第一行
+        self.check_for_row_in_list_table('1: Buy flowers')
 
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')  # (1)
-        self.assertIn('1: Buy flowers', [row.text for row in rows])
-
-# 页面中又显示了一个文本输入框，可以输入其他待办事项
-        # 他输入了“Give a gift to Lisi”
+        # 他又输入了“Give a gift to Lisi”并按回车
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        # 页面再次更新，她的清单中显示了这两个待办事项
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy flowers', [row.text for row in rows])
-        self.assertIn('2: Give a gift to Lisi', [row.text for row in rows])
+        # 【重点修改区 2】：使用新工具检查前两行都在不在
+        self.check_for_row_in_list_table('1: Buy flowers')
+        self.check_for_row_in_list_table('2: Give a gift to Lisi')
 
-        # 张三想知道这个网站是否会记住他的清单
-        # 他看到网站为他生成了一个唯一的URL
         self.fail('Finish the test!')
+
 if __name__ == '__main__':
     unittest.main()
-        # 页面再次更新，她的清单中显示了这两个待办事项
