01a8846bec9275b9b1ac32fd4068914445041542
杰拉德 <447121475@bupt.edu.cn>
2026-04-27 09:25:59 +0800

    创建 Item 模型并完成第一次数据库迁移

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    # 给模型增加一个 text 属性，类型是“文本类型”，默认值为空字符串
+    text = models.TextField(default='')
--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-
+from lists.models import Item
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
@@ -13,3 +13,21 @@ class HomePageTest(TestCase):
         
         # 2. 检查网页的 HTML 源码中，是否包含了我们刚刚提交的那句话
         self.assertIn('A new list item', response.content.decode())
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
