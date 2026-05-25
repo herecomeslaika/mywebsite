49b1214c6771bac4827c3ece10ce59bafd6ea5a6
杰拉德 <447121475@bupt.edu.cn>
2026-05-25 08:53:15 +0800

    重构：使用 include 提取 urls.py 到 lists 应用内部

---
+++ lists/urls.py
@@ -0,0 +1,9 @@
+﻿from django.urls import path, re_path
+from lists import views
+
+urlpatterns = [
+    # 注意：前面的 'lists/' 前缀都被去掉了，因为父级路由会处理它
+    path('new', views.new_list, name='new_list'),
+    re_path(r'^(\d+)/$', views.view_list, name='view_list'),
+    re_path(r'^(\d+)/add_item$', views.add_item, name='add_item'),
+]
--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -1,11 +1,11 @@
-from django.contrib import admin
-from django.urls import path, re_path  # 【注意】：这里新增了 re_path 的导入
-from lists import views
+from django.urls import path, include
+from lists import views as list_views  # 给 views 起个别名，避免混淆
+from lists import urls as list_urls    # 导入刚刚写好的子路由模块
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
-    path('lists/new', views.new_list, name='new_list'),
-    re_path(r'^lists/(\d+)/$', views.view_list, name='view_list'),
-    # 【新增下面这行】：专门处理追加事项的请求
-    re_path(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+    # 首页依然由主控台直接负责
+    path('', list_views.home_page, name='home'),
+    
+    # 核心重构：将所有以 lists/ 开头的请求，无脑转交给 list_urls 处理！
+    path('lists/', include(list_urls)),
 ]
